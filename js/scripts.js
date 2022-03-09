c416e4fae3554235b5f6deb802697b40d2582ff2
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2022-03-09 20:18:02 +0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*!
+* Start Bootstrap - Business Casual v7.0.6 (https://startbootstrap.com/theme/business-casual)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+*/
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("mainNav");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
